880c82f03600b75cc00009add03ba6ce9fda34f7
Mehdi M <hi@mehdi.cc>
2023-11-10 22:46:23 +0100

    Add repo edit link to pages footer

--- vitepress/.vitepress/config.mjs
+++ vitepress/.vitepress/config.mjs
@@ -31,5 +31,22 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/meduzen/blog' },
     ],
+
+    /**
+     * @todo: PR Vitepress so that returning no pattern should disabled the display of the link
+     * - https://github.com/vuejs/vitepress/blob/a482611d17197a0b7afc403891cd95f344e7a55f/src/client/theme-default/components/VPDocFooter.vue#L13C7-L13C15
+     * - https://github.com/vuejs/vitepress/blob/a482611d17197a0b7afc403891cd95f344e7a55f/src/client/theme-default/composables/edit-link.ts#L10
+     */
+    editLink: {
+      pattern: ({ filePath }) => {
+        // path starts by `articles/` or `notes/`
+        if (/^(articles|notes)\//.test(filePath)) {
+          return `https://github.com/meduzen/blog/edit/main/vitepress/${filePath}`
+        }
+
+        return undefined
+      },
+      text: 'Edit this page on GitHub',
+    },
   },
 })
