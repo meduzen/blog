ba6ec8dfcc3c35f5f83d7a34b61584a4b4b64036
Mehdi <9340937+meduzen@users.noreply.github.com>
2023-11-19 17:25:50 +0100

    Add more RSS feeds
    Summary:
    * Add separate feeds for notes and articles
    * Add feed with articles excerpt only
    * Add feed with articles excerpts and complete notes
    * Add new feeds to `size-limit`

--- vitepress/.vitepress/rss.mjs
+++ vitepress/.vitepress/rss.mjs
@@ -1,49 +1,99 @@
-import path from 'path'
-import { writeFileSync } from 'fs'
 import { Feed } from 'feed'
 import { createContentLoader } from 'vitepress'
+import { isArticle, isNote } from './utils/content-type.mjs'
 import { comparePublicationDate, isPublished } from './utils/frontmatter.mjs'
+import { baseFeedOptions, compareItemsDates, feedItem, writeFeed } from './utils/rss.mjs'
 
 /** @todo: should come from .env */
 const APP_URL = `https://blog.mehdi.cc`
 
 /** @param {import('vitepress').SiteConfig} config */
 export async function rss(config) {
-  /**
-   * https://www.rssboard.org/rss-profile
-   * https://github.com/jpmonette/feed
-   */
-  const feed = new Feed({
-    docs: 'https://www.rssboard.org/rss-specification',
-    link: APP_URL + 'link',
-    title: config.site.title,
-    description: config.site.description,
-    language: config.site.lang,
-    // image: 'https://blog.mehdi.cc/file.png',
-    // favicon: `${APP_URL}/favicon.ico`,
-    copyright: 'Copyright © 2023-present, Mehdi Merah',
-    feed: `${APP_URL}/feed.xml`,
-    ttl: 2880, // 1 day,
-  });
+  const feedOptions = baseFeedOptions(config)
 
-  (await createContentLoader(['articles/*.md', 'notes/*.md'], { excerpt: true, render: true }).load())
+  // Load content and turn it into feed items.
+
+  /** @type {import('vitepress').ContentData[]} */
+  const content = (await createContentLoader(['articles/*.md', 'notes/*.md'], { excerpt: true, render: true }).load())
     .filter(isPublished)
     .toSorted(comparePublicationDate)
-    .forEach(({ url, excerpt, frontmatter, html }) =>
-      feed.addItem({
-        title: frontmatter.title,
-        id: `${APP_URL}${url}`,
-        link: `${APP_URL}${url}`,
-        description: frontmatter.description || excerpt,
-        content: html,
-        date: frontmatter.publishedAt,
-        author: [{
-          name: 'Mehdi Merah',
-          link: 'https://mehdi.cc',
-          email: ' ', // hack, otherwise <author> is missing in RSS
-        }],
-      })
-    )
 
-  writeFileSync(path.join(config.outDir, 'feed.xml'), feed.rss2())
+  const notesItems = content.filter(isNote).map(feedItem)
+  const articlesItems = content.filter(isArticle).map(feedItem)
+  const articlesItemsExcerptOnly = content
+    .filter(isArticle)
+    .map(content => feedItem(content, { content: content.excerpt }))
+
+  /**
+   * Generate all feeds and store them on disk.
+   *
+   * - spec: https://www.rssboard.org/rss-specification
+   * - spec best practices: https://www.rssboard.org/rss-profile
+   * - `feed` package: https://github.com/jpmonette/feed
+   */
+
+  // Feed 1: full content
+
+  const feedWithEverything = new Feed({
+    title: 'Mehdi’s notes and articles',
+    description: config.site.description,
+    feed: `${APP_URL}/feed.xml`,
+    ...feedOptions,
+  })
+
+  feedWithEverything.items = [...notesItems, ...articlesItems].toSorted(compareItemsDates)
+
+  writeFeed('feed', feedWithEverything)
+
+  // Feed 2: notes
+
+  const feedWithNotes = new Feed({
+    title: 'Mehdi’s notes',
+    description: 'A chronological gathering of… notes.',
+    feed: `${APP_URL}/feed-notes-only.xml`,
+    ...feedOptions,
+  })
+
+  feedWithNotes.items = notesItems
+
+  writeFeed('feed-notes-only', feedWithNotes)
+
+  // Feed 3: articles
+
+  const feedWithArticles = new Feed({
+    title: 'Mehdi’s articles',
+    description: 'A chronological gathering of… articles.',
+    feed: `${APP_URL}/feed-articles-only.xml`,
+    ...feedOptions,
+  })
+
+  feedWithArticles.items = articlesItems
+
+  writeFeed('feed-articles-only', feedWithArticles)
+
+  // Feed 4: articles excerpts
+
+  const feedWithArticlesExcerpts = new Feed({
+    title: 'Mehdi’s articles (excerpts only)',
+    description: 'Excerpt of my articles.',
+    feed: `${APP_URL}/feed-articles-excerpts-only.xml`,
+    ...feedOptions,
+  })
+
+  feedWithArticlesExcerpts.items = articlesItemsExcerptOnly
+
+  writeFeed('feed-articles-excerpts-only', feedWithArticlesExcerpts)
+
+  // Feed 5: articles excerpts and notes
+
+  const feedWithArticlesExcerptsAndNotes = new Feed({
+    title: 'Mehdi’s light feed',
+    description: 'Articles excerpts and notes.',
+    feed: `${APP_URL}/feed-articles-excerpts-and-notes.xml`,
+    ...feedOptions,
+  })
+
+  feedWithArticlesExcerptsAndNotes.items = [...notesItems, ...articlesItemsExcerptOnly].toSorted(compareItemsDates)
+
+  writeFeed('feed-articles-excerpts-and-notes', feedWithArticlesExcerptsAndNotes)
 }
