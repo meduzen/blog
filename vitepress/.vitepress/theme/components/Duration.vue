d05617cd5660466cd715c85b7d4b8602d0061d22
Mehdi M <hi@mehdi.cc>
2024-09-07 13:48:46 +0200

    Add `<Duration>` component

---
+++ vitepress/.vitepress/theme/components/Duration.vue
@@ -0,0 +1,24 @@
+<script>
+const durationPropOption = {
+  validator: value => Number(value).toString() == value,
+  required: false,
+  default: 0,
+}
+</script>
+
+<script setup>
+import { duration } from 'datetime-attribute'
+
+const props = defineProps({
+  w: durationPropOption,
+  d: durationPropOption,
+  m: durationPropOption,
+})
+</script>
+
+<template>
+  <time :datetime="duration(props)">
+    <!-- @todo Consider: if no slot provided, generate the duration. -->
+    <slot></slot>
+  </time>
+</template>
